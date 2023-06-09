38ae48883db326ff05be9c24a81cd7ac2cabac04
Atlas Sproson <a.sproson@outlook.com>
2023-06-09 14:29:31 +0100

    Fixes

--- tensorReact/tensorreact/src/App.tsx
+++ tensorReact/tensorreact/src/App.tsx
@@ -8,18 +8,18 @@ function App() {
   const [tensorOutput, setTensorOutput] = useState()
 
   const triggerModel = useCallback(() => {
-    // Define a model for linear regression.
+    // Define a model for linear regression
     const model = tf.sequential();
     model.add(tf.layers.dense({units: 1, inputShape: [1]}));
   
-    // Prepare the model for training: Specify the loss and the optimizer.
+    // Prepare the model for training: Specify the loss and the optimize
     model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
   
-    // Generate some synthetic data for training.
+    // Generate some synthetic data for training
     const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
     const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
   
-    // Train the model using the data.
+    // Train the model using the data
     model.fit(xs, ys).then(() => {
     // Use the model to do inference on a data point the model hasn't seen before:
     let res = model.predict(tf.tensor2d([10], [1, 1]))
